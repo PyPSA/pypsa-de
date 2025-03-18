a4d2c32c10b3f24dd2319fed9a7e1b82946ca834
Philip Worschischek <philip.worschischek@aggm.at>
2025-03-18 10:24:43 +0000

    register model kwargs

--- scripts/solve_network.py
+++ scripts/solve_network.py
@@ -1005,6 +1005,8 @@ def solve_network(n, config, params, solving, **kwargs):
     kwargs["assign_all_duals"] = cf_solving.get("assign_all_duals", False)
     kwargs["io_api"] = cf_solving.get("io_api", None)
 
+    kwargs["model_kwargs"] = cf_solving.get("model_kwargs", {})
+
     if kwargs["solver_name"] == "gurobi":
         logging.getLogger("gurobipy").setLevel(logging.CRITICAL)
 
