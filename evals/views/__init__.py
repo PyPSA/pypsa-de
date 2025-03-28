9ff3e4b0794debc590224d2fcda9543ea775c14a
Johannes Misensky <johannes.misensky@aggm.at>
2025-03-28 10:46:03 +0000

    fixed merge conflict

--- evals/views/__init__.py
+++ evals/views/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """Contains the views module."""
 
+from evals.views.capacity.electricity_capacity import view_electricity_capacities
 from evals.views.capacity.heat_capacity import view_heat_capacity
 
-__all__ = [
-    "view_heat_capacity",
-]
+__all__ = ["view_heat_capacity", "view_electricity_capacities"]
