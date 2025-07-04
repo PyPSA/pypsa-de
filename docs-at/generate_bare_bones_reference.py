f606336f65239363877f527833426cd683bbe985
Philip Worschischek <philip.worschischek@aggm.at>
2025-07-04 10:25:51 +0000

    clean up

--- docs-at/generate_bare_bones_reference.py
+++ docs-at/generate_bare_bones_reference.py
@@ -3,10 +3,11 @@ import pathlib
 
 def py_to_md_file(subdir):
     """Generate all required markdown files in a subdirectory."""
-    directory = SRC_DIR / subdir
-    target = ROOTDIR / "docs-at" / "reference" / "evals" / subdir
+    source = SRC_DIR / subdir
+    package = SRC_DIR.stem
+    target = ROOTDIR / "docs-at" / "reference" / package / subdir
 
-    for p in directory.iterdir():
+    for p in source.iterdir():
         if p.suffix != ".py":
             continue
         if p.stem == "__init__" and not subdir:  # special index file
@@ -14,14 +15,14 @@ def py_to_md_file(subdir):
 
         if p.stem == "__init__":
             new = target / "index.md"
-            module = f"evals.{subdir}" if subdir else "evals"
+            module = f"{package}.{subdir}" if subdir else package
         else:
             new = target / (p.stem + ".md")
-            module = f"evals.{subdir}.{p.stem}" if subdir else f"evals.{p.stem}"
-        # print(str(new), f"::: {module}")
+            module = f"{package}.{subdir}.{p.stem}" if subdir else f"{package}.{p.stem}"
+
         target.mkdir(exist_ok=True)
         new.touch(exist_ok=True)
-        new.write_text(f"::: {module}")
+        new.write_text(f"::: {module}")  # overwrites all contents
 
 
 if __name__ == "__main__":
