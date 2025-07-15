4cefdf0218790ae7ec30d90a6acce45c6c162a35
Philip Worschischek <philip.worschischek@aggm.at>
2025-07-15 07:08:26 +0000

    clean tests

--- test/test_iamc_exporter.py
+++ test/test_iamc_exporter.py
@@ -1,5 +1,9 @@
+from itertools import product
+
 import pandas as pd
 import pytest
+
+from test.views.test_evals import Constants
 
 
 @pytest.fixture(scope="module")
@@ -22,18 +26,28 @@ def exported_variables(results) -> pd.Series:
         index_col=[0, 1, 2, 3, 4],
     )
     iamc.columns.name = "Year"
-    return iamc.stack()  # long format data series
+    return iamc.stack()
 
 
 @pytest.mark("integration")
-def test_biogas_balances(df: pd.DataFrame, threshold: float = 0.1) -> None:
+@pytest.mark.parametrize(
+    ("year", "region"),
+    [
+        product(Constants.YEARS, Constants.LOCATIONS),
+    ],
+)
+def test_biogas_balances(
+    exported_variables: pd.DataFrame, year, region, threshold: float = 0.1
+) -> None:
     """
     Test if Solid Biomass supply and demand are balanced.
 
     Parameters
     ----------
-    df
+    exported_variables
         The IAMC variables table.
+    year
+    region
     threshold
         The allowed imbalance in TWh.
 
@@ -42,29 +56,28 @@ def test_biogas_balances(df: pd.DataFrame, threshold: float = 0.1) -> None:
     :
     """
     bc = "Biomass"
+    ds = (
+        exported_variables.query("Year == @year and Region == @region")
+        .drop("billion EUR2020", level="Unit")
+        .droplevel(["Year", "Region", "Scenario", "Model"])
+        .div(1e6)
+    )  # to TWh for readability
 
-    for (year, region), ds in df.groupby(["Year", "Region"]):
-        ds = ds.drop("billion EUR2020", level="Unit").droplevel(
-            ["Year", "Region", "Scenario", "Model"]
-        )
-        data = ds.filter(like=bc) / 1e6
-        primary = data.filter(regex=rf"^\('Primary Energy\|{bc}")
-        secondary_supply = data.filter(regex=rf"^\('Secondary Energy\|{bc}")
-        secondary_demand = data.filter(
-            regex=rf"^\('Secondary Energy\|[a-zA-Z0-9\s]*\|{bc}"
-        )
-        final_demand = data.filter(regex=rf"^\('Final Energy\|{bc}")
+    primary = ds.filter(regex=rf"^\('Primary Energy\|{bc}")
+    secondary_supply = ds.filter(regex=rf"^\('Secondary Energy\|{bc}")
+    secondary_demand = ds.filter(regex=rf"^\('Secondary Energy\|[a-zA-Z0-9\s]*\|{bc}")
+    final_demand = ds.filter(regex=rf"^\('Final Energy\|{bc}")
 
-        # ambient heat must be excluded because it is not a demand
-        secondary_demand = secondary_demand.drop(
-            secondary_demand.filter(like="Ambient Heat").index
-        )
+    # ambient heat must be excluded because it is not a demand
+    secondary_demand = secondary_demand.drop(
+        secondary_demand.filter(like="Ambient Heat").index
+    )
 
-        balance = (
-            primary.sum()
-            + secondary_supply.sum()
-            - secondary_demand.sum()
-            - final_demand.sum()
-        )
+    balance = (
+        primary.sum()
+        + secondary_supply.sum()
+        - secondary_demand.sum()
+        - final_demand.sum()
+    )
 
-        assert abs(balance) <= threshold, f"Imbalances detected: {balance:.3f} TWh"
+    assert abs(balance) <= threshold, f"Imbalances detected: {balance:.3f} TWh"
