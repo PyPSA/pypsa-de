11714732d6f21f06d68f7e1d7153db218eab9dc5
Michael Lindner <michaellindner@posteo.de>
2025-07-24 18:41:18 +0200

    rewrite fix capacities

--- scripts/pypsa-de/solve_regret.py
+++ scripts/pypsa-de/solve_regret.py
@@ -16,8 +16,7 @@ logger = logging.getLogger(__name__)
 
 
 def fix_capacities(realization, decision):
-    realization.optimize.fix_optimal_capacities()  # also sets p_nom = p_nom_opt
-    decision.optimize.fix_optimal_capacities()
+    n = realization.copy()
 
     nominal_attrs = {
         "generators": "p_nom",
@@ -26,22 +25,47 @@ def fix_capacities(realization, decision):
         "stores": "e_nom",
     }
 
-    realization.links.index.intersection(decision.links.index)
-
     for name, attr in nominal_attrs.items():
-        real = getattr(realization, name)
+        new = getattr(n, name)
         deci = getattr(decision, name)
 
-        common = real.index.intersection(deci.index).difference(
-            real.query("carrier == 'BEV charger'").index
-        )
-        if not real.index.equals(deci.index):
-            logger.warning(
+        if not new.index.equals(deci.index):
+            logger.error(
                 f"Indices of {name} in realization and decision networks do not match. "
                 "This may lead to unexpected results."
             )
+            # raise ValueError("Indices of realization and decision networks do not match.")
 
-        real.loc[common, attr] = deci.loc[common, attr]
+        common_i = new.index.intersection(deci.index).difference(
+            new.query("carrier == 'BEV charger'").index
+        )  # Exclude BEV chargers from modification because the p_nom are taken from UBA
+
+        extendable_i = new.query(f"{attr}_extendable").index
+
+        if not deci.query(f"{attr}_opt > {attr}_max").empty:
+            logger.error(
+                f"Decision network have {name} with {attr}_opt > {attr}_max. "
+                "This may lead to unexpected results."
+                f"These assets are: {deci.query(f'{attr}_opt > {attr}_max').index.tolist()}"
+            )
+            _idx = deci.query(f"{attr}_opt > {attr}_max").index
+            deci.loc[_idx, attr + "_max"] = deci.loc[_idx, attr + "_opt"] = np.minimum(
+                deci.loc[_idx, attr + "_max"], deci.loc[_idx, attr + "_opt"]
+            )
+
+        if not deci.query(f"{attr}_min > {attr}_opt").empty:
+            logger.error(
+                f"Decision network have {name} with {attr}_min > {attr}_opt. "
+                "This may lead to unexpected results."
+                f"These assets are: {deci.query(f'{attr}_min > {attr}_opt').index.tolist()}"
+            )
+        new.loc[common_i, attr] = deci.loc[common_i, attr]
+        new.loc[common_i, attr + "_opt"] = deci.loc[common_i, attr + "_opt"]
+        new.loc[common_i, attr + "_min"] = deci.loc[common_i, attr + "_min"]
+        new.loc[common_i, attr + "_max"] = deci.loc[common_i, attr + "_max"]
+
+        # Ideally nothing should be extendable
+        new.loc[extendable_i, attr + "_extendable"] = False
 
         if name == "links":
             virtual_links = [
@@ -58,20 +82,29 @@ def fix_capacities(realization, decision):
                 "shipping oil",
                 "kerosene for aviation",
                 "agriculture machinery oil",
+                # "urban central water pits charger", # This should have no effect on results since e_nom of the water pits store remains fixed and the energy to power ratio constraints force them to be the same, however it may avoid infeasibilitiesl
             ]
+            # TODO double check if these are all needed
             essential_links = [
                 "SMR",
                 "waste CHP",
+                "biogas to gas",
             ]
+            # TODO double check if these are all needed
             bottleneck_links = [
                 "electricity distribution grid",
                 "methanolisation",
+                "rural gas boiler",
+                "urban decentral gas boiler",
             ]
             links_to_free = virtual_links + essential_links + bottleneck_links
-            real.loc[real.carrier.isin(links_to_free), "p_nom_extendable"] = True
-            real.loc[real.carrier.isin(links_to_free), "p_nom_min"] = real.loc[
-                real.carrier.isin(links_to_free), "p_nom"
-            ]
+            _idx = new.carrier.isin(links_to_free).index.intersection(extendable_i)
+            new.loc[_idx, "p_nom_extendable"] = True
+            # For essential links and bottleneck links allow more, but not less
+            _idx = new.carrier.isin(
+                essential_links + bottleneck_links
+            ).index.intersection(extendable_i)
+            new.loc[_idx, "p_nom_min"] = new.loc[_idx, "p_nom_opt"]
 
         if name == "generators":
             fuels = [
@@ -86,15 +119,39 @@ def fix_capacities(realization, decision):
                 "rural heat vent",
                 "urban decentral heat vent",
             ]
-            real.loc[real.carrier.isin(fuels + vents), "p_nom_extendable"] = True
-            real.loc[real.carrier.isin(fuels), "p_nom_min"] = real.loc[
-                real.carrier.isin(fuels), "p_nom"
-            ]
+            _idx = new.carrier.isin(fuels + vents).index.intersection(extendable_i)
+            new.loc[_idx, "p_nom_extendable"] = True
+            # For fuels allow more, but not less
+            _idx = new.carrier.isin(fuels).index.intersection(extendable_i)
+            new.loc[_idx, "p_nom_min"] = new.loc[_idx, "p_nom_opt"]
 
         if name == "stores":
-            real.loc[real.carrier == "co2", "e_nom_extendable"] = True
+            # there is only one co2 atmosphere store which is always extendable, hence no intersection with extendable_i needed
+            new.loc[new.carrier == "co2", "e_nom_extendable"] = True
 
-    return realization
+            # Just making sure that the defaults are set correctly
+            assert (new.loc[new.carrier == "co2", "e_nom_min"] == 0).all()
+            assert (new.loc[new.carrier == "co2", "e_nom_max"] == np.inf).all()
+            assert (new.loc[new.carrier == "co2", "e_nom"] == 0).all()
+
+            # TODO double check if these are all needed
+            co2_stores = ["co2 stored", "co2 sequestered"]
+            _idx = new.carrier.isin(co2_stores).index.intersection(extendable_i)
+            new.loc[_idx, "e_nom_extendable"] = True
+            # Allow less, but not more
+            new.loc[_idx, "e_nom_max"] = new.loc[_idx, "e_nom_opt"]
+
+            _idx = new.carrier.isin(["urban central water pits"]).index.intersection(
+                extendable_i
+            )
+            new.loc[_idx, "e_nom_extendable"] = (
+                True  # This should have no effect on results as long as p_nom of the water pits charger remains fixed and the energy to power ratio constraints force them to be the same, however it may avoid infeasibilities
+            )
+            new.loc[_idx, "e_nom_min"] = 0
+            new.loc[_idx, "e_nom_max"] = np.inf
+            new.loc[_idx, "e_nom"] = 0
+
+    return n
 
 
 if __name__ == "__main__":
