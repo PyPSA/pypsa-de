5b82337e232e2a2f0d8a5ce381d6a1ff67116b4e
Philip Worschischek <philip.worschischek@aggm.at>
2025-08-11 12:35:06 +0000

    finished sankey mapping

--- evals/views/sankey.py
+++ evals/views/sankey.py
@@ -51,15 +51,149 @@ def collect_imbalances(supply, demand):
 
     if len(bc_in) > 1:
         to_concat = []
-        for bus_carrier_demand in bc_in:
-            demand_bc = filter_by(demand, bus_carrier=bus_carrier_demand)
+        for _bc in bc_in:
+            demand_bc = filter_by(demand, bus_carrier=_bc)
             demand_share = demand_bc.sum() / demand.sum()
             supply_bc = supply * demand_share
-            to_concat.append(
-                _process_single_input_link(supply_bc, demand_bc, bus_carrier_demand)
-            )
+            to_concat.append(_process_single_input_link(supply_bc, demand_bc, _bc))
+            mapper = {
+                v: f"{v} from {_bc}" for v in supply_bc.index.unique("bus_carrier")
+            }
+            to_concat.append(rename_aggregate(supply_bc, mapper, level="bus_carrier"))
         return pd.concat(to_concat)
     return _process_single_input_link(supply, demand, bc_in.item())
+
+
+def get_supply(networks, transmission_comps, transmission_carrier):
+    supply = (
+        collect_myopic_statistics(
+            networks,
+            statistic="supply",
+            aggregate_components=None,
+        )
+        .pipe(
+            filter_by,
+            component=transmission_comps,
+            carrier=transmission_carrier,
+            exclude=True,
+        )
+        .pipe(
+            drop_from_multtindex_by_regex, "co2|process emissions", level="bus_carrier"
+        )
+        .pipe(
+            rename_aggregate,
+            {
+                "hydro": "hydro supply",
+                "PHS": "PHS supply",
+                "H2 Store": "H2 Store supply",
+                "gas": "gas supply",
+            },
+        )
+    )
+    supply.attrs["unit"] = "MWh"
+
+    return supply
+
+
+def get_demand(networks, transmission_comps, transmission_carrier, unit):
+    demand = (
+        collect_myopic_statistics(
+            networks,
+            statistic="withdrawal",
+            aggregate_components=None,
+        )
+        .pipe(
+            filter_by,
+            component=transmission_comps,
+            carrier=transmission_carrier,
+            exclude=True,
+        )
+        .pipe(
+            drop_from_multtindex_by_regex, "co2|process emissions", level="bus_carrier"
+        )
+        .pipe(
+            rename_aggregate,
+            {
+                "hydro": "hydro demand",
+                "PHS": "PHS demand",
+                "H2 Store": "H2 Store demand",
+                "gas": "gas demand",
+            },
+        )
+        .mul(-1)
+    )
+    demand.attrs["unit"] = unit
+
+    return demand
+
+
+def net_distribution_grid_losses(supply, demand):
+    grid_losses = (
+        filter_by(supply, carrier="electricity distribution grid")
+        .groupby(IDX)
+        .sum()
+        .add(
+            filter_by(demand, carrier="electricity distribution grid")
+            .groupby(IDX)
+            .sum()
+        )
+    )
+    grid_losses = insert_index_level(grid_losses, "losses", "bus_carrier", pos=4)
+    supply.drop("electricity distribution grid", level="carrier", inplace=True)
+    demand.drop("electricity distribution grid", level="carrier", inplace=True)
+
+    return grid_losses
+
+
+def get_trade_statistics(networks, transmission_comps, transmission_carrier, unit):
+    trade_statistics = []
+    for scope, direction, alias in [
+        (TradeTypes.FOREIGN, "import", Group.import_foreign),
+        (TradeTypes.FOREIGN, "export", Group.export_foreign),
+        (TradeTypes.DOMESTIC, "import", Group.import_domestic),
+        (TradeTypes.DOMESTIC, "export", Group.export_domestic),
+    ]:
+        trade = (
+            collect_myopic_statistics(
+                networks,
+                statistic="trade_energy",
+                scope=scope,
+                direction=direction,
+                aggregate_components=None,
+            )
+            # the trade statistic wrongly finds transmission between EU -> country buses.
+            # Those are dropped by the filter_by statement.
+            .pipe(
+                filter_by,
+                component=transmission_comps,
+                carrier=transmission_carrier,
+            )
+            .pipe(drop_from_multtindex_by_regex, "co2", level="bus_carrier")
+            .pipe(rename_aggregate, alias)
+            .abs()
+        )
+        trade.attrs["unit"] = unit
+        trade_statistics.append(trade)
+
+    return trade_statistics
+
+
+def get_link_losses(supply, demand):
+    link_losses = []
+    link_supply_carrier = filter_by(supply, component="Link").index.unique("carrier")
+    link_demand_carrier = filter_by(demand, component="Link").index.unique("carrier")
+    link_carrier = link_supply_carrier.union(link_demand_carrier)
+    for carrier in link_carrier:
+        link_supply = filter_by(supply, carrier=carrier, component="Link")
+        link_demand = filter_by(demand, carrier=carrier, component="Link")
+        # balance = link_supply.droplevel("bus_carrier").add(link_demand.droplevel("bus_carrier")).groupby(["year", "component", "location", "carrier"]).sum()
+        if link_supply.empty or link_demand.empty:
+            print(f"Skipping carrier '{carrier}' due to empty supply or demand.")
+            continue
+        link_losses.append(collect_imbalances(link_supply, link_demand))
+        # demand.drop(link_demand.index, inplace=True)
+
+    return link_losses
 
 
 def view_sankey(
@@ -81,64 +215,21 @@ def view_sankey(
         storage_links,
     ) = _parse_view_config_items(networks, config)
 
-    supply = (
-        collect_myopic_statistics(
-            networks,
-            statistic="supply",
-            aggregate_components=None,
-        )
-        .pipe(
-            filter_by,
-            component=transmission_comps,
-            carrier=transmission_carrier,
-            exclude=True,
-        )
-        .pipe(
-            drop_from_multtindex_by_regex, "co2|process emissions", level="bus_carrier"
-        )
-        .pipe(rename_aggregate, {"hydro": "hydro supply", "PHS": "PHS supply"})
+    supply = get_supply(networks, transmission_comps, transmission_carrier)
+    demand = get_demand(
+        networks, transmission_comps, transmission_carrier, unit=supply.attrs["unit"]
     )
-    supply.attrs["unit"] = "MWh"
-
-    demand = (
-        collect_myopic_statistics(
-            networks,
-            statistic="withdrawal",
-            aggregate_components=None,
-        )
-        .pipe(
-            filter_by,
-            component=transmission_comps,
-            carrier=transmission_carrier,
-            exclude=True,
-        )
-        .pipe(
-            drop_from_multtindex_by_regex, "co2|process emissions", level="bus_carrier"
-        )
-        .pipe(rename_aggregate, {"hydro": "hydro demand", "PHS": "PHS demand"})
-        .mul(-1)
-    )
-    demand.attrs["unit"] = supply.attrs["unit"]
 
     # todo:
     #  - calculate regional oil import from regional oil demand
     #  - calculate regional NH3 Load from regional NH3 production
     #  - calculate StorageUnit losses
-
-    grid_losses = (
-        filter_by(supply, carrier="electricity distribution grid")
-        .groupby(IDX)
-        .sum()
-        .add(
-            filter_by(demand, carrier="electricity distribution grid")
-            .groupby(IDX)
-            .sum()
-        )
+    #  - harmonize V2G amounts
+    grid_losses = net_distribution_grid_losses(supply, demand)
+    trade_statistics = get_trade_statistics(
+        networks, transmission_comps, transmission_carrier, unit=supply.attrs["unit"]
     )
-    grid_losses = insert_index_level(grid_losses, "losses", "bus_carrier", pos=4)
-    supply.drop("electricity distribution grid", level="carrier", inplace=True)
-    demand.drop("electricity distribution grid", level="carrier", inplace=True)
-
+    link_losses = get_link_losses(supply, demand)
     # storage_systems = (
     #     "rural water tanks",
     #     "urban central water tanks",
@@ -201,49 +292,6 @@ def view_sankey(
     #         )
     #     supply.drop(industry_supply.index, inplace=True)
     #     demand.drop(industry_demand.index, inplace=True)
-
-    link_losses = []
-    link_supply_carrier = filter_by(supply, component="Link").index.unique("carrier")
-    link_demand_carrier = filter_by(demand, component="Link").index.unique("carrier")
-    link_carrier = link_supply_carrier.union(link_demand_carrier)
-    for carrier in link_carrier:
-        link_supply = filter_by(supply, carrier=carrier, component="Link")
-        link_demand = filter_by(demand, carrier=carrier, component="Link")
-        # balance = link_supply.droplevel("bus_carrier").add(link_demand.droplevel("bus_carrier")).groupby(["year", "component", "location", "carrier"]).sum()
-        if link_supply.empty or link_demand.empty:
-            print(f"Skipping carrier '{carrier}' due to empty supply or demand.")
-            continue
-        link_losses.append(collect_imbalances(link_supply, link_demand))
-        # demand.drop(link_demand.index, inplace=True)
-
-    trade_statistics = []
-    for scope, direction, alias in [
-        (TradeTypes.FOREIGN, "import", Group.import_foreign),
-        (TradeTypes.FOREIGN, "export", Group.export_foreign),
-        (TradeTypes.DOMESTIC, "import", Group.import_domestic),
-        (TradeTypes.DOMESTIC, "export", Group.export_domestic),
-    ]:
-        trade = (
-            collect_myopic_statistics(
-                networks,
-                statistic="trade_energy",
-                scope=scope,
-                direction=direction,
-                aggregate_components=None,
-            )
-            # the trade statistic wrongly finds transmission between EU -> country buses.
-            # Those are dropped by the filter_by statement.
-            .pipe(
-                filter_by,
-                component=transmission_comps,
-                carrier=transmission_carrier,
-            )
-            .pipe(drop_from_multtindex_by_regex, "co2", level="bus_carrier")
-            .pipe(rename_aggregate, alias)
-            .abs()
-        )
-        trade.attrs["unit"] = supply.attrs["unit"]
-        trade_statistics.append(trade)
 
     exporter = Exporter(
         statistics=[supply, demand, grid_losses]
