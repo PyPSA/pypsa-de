74a547185fc8d039abdc6474f4e71ae07f9a1894
Michael Lindner <michaellindner@posteo.de>
2025-08-13 09:45:08 +0200

    large refactor

--- scripts/pypsa-de/solve_regret.py
+++ scripts/pypsa-de/solve_regret.py
@@ -1,4 +1,5 @@
 import logging
+import pathlib
 
 import numpy as np
 import pypsa
@@ -31,44 +32,105 @@ def check_matching_components(new, deci, name, attr):
         )
 
 
-def fix_capacities_strict(realization, decision):
-    n = realization.copy()
+def _unfix_bottlenecks(new, deci, name, extendable_i):
+    if name == "links":
+        virtual_links = [
+            "land transport oil",
+            "land transport fuel cell",
+            "solid biomass for industry",
+            "gas for industry",
+            "industry methanol",
+            "naphtha for industry",
+            "process emissions",
+            "coal for industry",
+            "H2 for industry",
+            "shipping methanol",
+            "shipping oil",
+            "kerosene for aviation",
+            "agriculture machinery oil",
+            # "co2 sequestered",
+        ]
 
-    nominal_attrs = {
-        "generators": "p_nom",
-        "lines": "s_nom",
-        "links": "p_nom",
-        "stores": "e_nom",
-    }
+        _idx = new.loc[new.carrier.isin(virtual_links)].index.intersection(extendable_i)
+        # Virtual links are free
+        new.loc[_idx, "p_nom_extendable"] = True
 
-    for name, attr in nominal_attrs.items():
-        new = getattr(n, name)
-        deci = getattr(decision, name)
+        bottleneck_links = [
+            "SMR",
+            "rural gas boiler",
+            "urban decentral gas boiler",
+            # For 2035
+            # "rural biomass boiler",
+            # "urban decentral biomass boiler",
+        ]
+        # Bottleneck links can be extended, but not reduced
+        _idx = new.loc[new.carrier.isin(bottleneck_links)].index.intersection(
+            extendable_i
+        )
+        new.loc[_idx, "p_nom_extendable"] = True
+        new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
 
-        check_matching_components(new, deci, name, attr)
+        # No limits for waste burning outside DE
+        _idx = new.query(
+            "carrier == 'HVC to air' and not index.str.startswith('DE')"
+        ).index.intersection(extendable_i)
+        new.loc[_idx, "p_nom_extendable"] = True
+        new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
+        # Tight limits inside DE
+        _idx = new.query(
+            "carrier == 'waste CHP' and index.str.startswith('DE')"
+        ).index.intersection(extendable_i)
+        new.loc[_idx, "p_nom_extendable"] = True
+        new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
+        # new.loc[_idx, "p_nom_max"] = deci.loc[_idx, "p_nom_opt"] + 0.1
 
-        common_i = new.query("carrier != 'BEV charger'").index.intersection(
-            deci.query("carrier != 'BEV charger'").index
-        )  # Exclude BEV chargers from modification because the p_nom are taken from UBA
-        extendable_i = new.query(f"{attr}_extendable").index
+        _idx = new.query(
+            "carrier == 'electricity distribution grid'"
+        ).index.intersection(extendable_i)
+        new.loc[_idx, "p_nom_extendable"] = True
+        new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
+        # new.loc[_idx, "p_nom_max"] = deci.loc[_idx, "p_nom_opt"] + 0.1
 
-        # Copy all assets (maybe this should only be done for current planning horizon)
-        new.loc[common_i, attr] = deci.loc[common_i, attr]
-        new.loc[common_i, attr + "_opt"] = deci.loc[common_i, attr + "_opt"]
-        new.loc[common_i, attr + "_min"] = deci.loc[common_i, attr + "_min"]
-        new.loc[common_i, attr + "_max"] = deci.loc[common_i, attr + "_max"]
+    # if name == "lines":
+    #     # For lines allow only a minimal extension, to avoid powerflow constraint issues
+    #     new.loc[extendable_i, "s_nom_extendable"] = True
 
-        # Fix everything...
-        new.loc[extendable_i, attr + "_extendable"] = False
-        new.loc[extendable_i, attr] = new.loc[extendable_i, attr + "_opt"]
-        if name == "stores":
-            # there is only one co2 atmosphere store which should always be extendable, hence no intersection with extendable_i needed
-            _idx = new.query("carrier == 'co2'").index
-            new.loc[_idx, "e_nom_extendable"] = True
-    return n
+    #     new.loc[extendable_i, "s_nom_min"] = deci.loc[extendable_i, "s_nom_opt"]
+    #     new.loc[extendable_i, "s_nom_max"] = deci.loc[extendable_i, "s_nom_opt"] + 10
+
+    if name == "generators":
+        fuels = [
+            "lignite",
+            "coal",
+            "oil primary",
+            "uranium",
+            "gas primary",
+        ]
+        vents = [
+            "urban central heat vent",
+            "rural heat vent",
+            "urban decentral heat vent",
+        ]
+        _idx = new.loc[new.carrier.isin(fuels + vents)].index.intersection(extendable_i)
+        new.loc[_idx, "p_nom_extendable"] = True
+
+    # if name == "stores":
+    # For co2 sequestered no limits are needed since the global constraints is active
+    # _idx = new.query("carrier == 'co2 sequestered'").index.intersection(
+    #     extendable_i
+    # )
+    # new.loc[_idx, "e_nom_extendable"] = True
+    # new.loc[_idx, "e_nom_max"] = deci.loc[_idx, "e_nom_opt"] + 1
+
+    # _idx = new.query("carrier == 'co2 stored'").index.intersection(
+    #     extendable_i
+    # )
+    # new.loc[_idx, "e_nom_extendable"] = True
+
+    return
 
 
-def fix_capacities(realization, decision):
+def fix_capacities(realization, decision, scope="", strict=False):
     n = realization.copy()
 
     nominal_attrs = {
@@ -101,7 +163,9 @@ def fix_capacities(realization, decision):
         common_i = new.query("carrier != 'BEV charger'").index.intersection(
             deci.query("carrier != 'BEV charger'").index
         )  # Exclude BEV chargers from modification because the p_nom are taken from UBA
-        extendable_i = new.query(f"{attr}_extendable").index
+        extendable_i = new.query(
+            f"{attr}_extendable and index.str.startswith('{scope}')"
+        ).index
 
         # Copy all assets (maybe this should only be done for current planning horizon)
         new.loc[common_i, attr] = deci.loc[common_i, attr]
@@ -113,111 +177,18 @@ def fix_capacities(realization, decision):
         new.loc[extendable_i, attr + "_extendable"] = False
         new.loc[extendable_i, attr] = new.loc[extendable_i, attr + "_opt"]
 
-        if name == "links":
-            virtual_links = [
-                "land transport oil",
-                "land transport fuel cell",
-                "solid biomass for industry",
-                "gas for industry",
-                "industry methanol",
-                "naphtha for industry",
-                "process emissions",
-                "coal for industry",
-                "H2 for industry",
-                "shipping methanol",
-                "shipping oil",
-                "kerosene for aviation",
-                "agriculture machinery oil",
-                # "co2 sequestered",
-            ]
-
-            _idx = new.loc[new.carrier.isin(virtual_links)].index.intersection(
-                extendable_i
-            )
-            # Virtual links are free
-            new.loc[_idx, "p_nom_extendable"] = True
-
-            bottleneck_links = [
-                "SMR",
-                "rural gas boiler",
-                "urban decentral gas boiler",
-                # For 2035
-                "rural biomass boiler",
-                "urban decentral biomass boiler",
-            ]
-            # Bottleneck links can be extended, but not reduced
-            _idx = new.loc[new.carrier.isin(bottleneck_links)].index.intersection(
-                extendable_i
-            )
-            new.loc[_idx, "p_nom_extendable"] = True
-            new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
-
-            # No limits for waste burning outside DE
-            _idx = new.query(
-                "carrier == 'HVC to air' and not index.str.startswith('DE')"
-            ).index.intersection(extendable_i)
-            new.loc[_idx, "p_nom_extendable"] = True
-            new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
-            # Tight limits inside DE
-            _idx = new.query(
-                "carrier == 'waste CHP' and index.str.startswith('DE')"
-            ).index.intersection(extendable_i)
-            new.loc[_idx, "p_nom_extendable"] = True
-            new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
-            # new.loc[_idx, "p_nom_max"] = deci.loc[_idx, "p_nom_opt"] + 0.1
-
-            _idx = new.query(
-                "carrier == 'electricity distribution grid'"
-            ).index.intersection(extendable_i)
-            new.loc[_idx, "p_nom_extendable"] = True
-            new.loc[_idx, "p_nom_min"] = deci.loc[_idx, "p_nom_opt"]
-            # new.loc[_idx, "p_nom_max"] = deci.loc[_idx, "p_nom_opt"] + 0.1
-
-        # if name == "lines":
-        #     # For lines allow only a minimal extension, to avoid powerflow constraint issues
-        #     new.loc[extendable_i, "s_nom_extendable"] = True
-
-        #     new.loc[extendable_i, "s_nom_min"] = deci.loc[extendable_i, "s_nom_opt"]
-        #     new.loc[extendable_i, "s_nom_max"] = deci.loc[extendable_i, "s_nom_opt"] + 10
-
-        if name == "generators":
-            fuels = [
-                "lignite",
-                "coal",
-                "oil primary",
-                "uranium",
-                "gas primary",
-            ]
-            vents = [
-                "urban central heat vent",
-                "rural heat vent",
-                "urban decentral heat vent",
-            ]
-            _idx = new.loc[new.carrier.isin(fuels + vents)].index.intersection(
-                extendable_i
-            )
-            new.loc[_idx, "p_nom_extendable"] = True
+        if not strict:
+            _unfix_bottlenecks(new, deci, name, extendable_i)
 
         if name == "stores":
             # there is only one co2 atmosphere store which should always be extendable, hence no intersection with extendable_i needed
             _idx = new.query("carrier == 'co2'").index
             new.loc[_idx, "e_nom_extendable"] = True
 
-            # For co2 sequestered no limits are needed since the global constraints is active
-            # _idx = new.query("carrier == 'co2 sequestered'").index.intersection(
-            #     extendable_i
-            # )
-            # new.loc[_idx, "e_nom_extendable"] = True
-            # new.loc[_idx, "e_nom_max"] = deci.loc[_idx, "e_nom_opt"] + 1
-
-            # _idx = new.query("carrier == 'co2 stored'").index.intersection(
-            #     extendable_i
-            # )
-            # new.loc[_idx, "e_nom_extendable"] = True
-
         # Above several assets are switched to extendable again, for these the p_nom value is restored, i.e. set to the value from the decision network
         _idx = new.query(f"{attr}_extendable").index.intersection(extendable_i)
         new.loc[_idx, attr] = deci.loc[_idx, attr]
+
     return n
 
 
@@ -230,7 +201,7 @@ if __name__ == "__main__":
             sector_opts="none",
             planning_horizons="2035",
             decision="LowDemand",
-            run="LowDemand",
+            run="AriadneDemand",
         )
 
     configure_logging(snakemake)
@@ -243,8 +214,6 @@ if __name__ == "__main__":
 
     logger.info("Loading realization and decision networks")
 
-    import pathlib
-
     # Touch output file to ensure it exists
     pathlib.Path(snakemake.output.regret_network).touch()
 
@@ -256,68 +225,14 @@ if __name__ == "__main__":
         "mem_logging_frequency", 30
     )
 
-    n = fix_capacities_strict(realization, decision)
+    n = fix_capacities(realization, decision, scope="DE", strict=False)
 
     n_pre = n.copy()
-
-    # logger.info("Adding CO2 removal service outside DE.")
-    # n.add("Carrier", "CO2 removal service")
-
-    # n.add(
-    #     "Generator",
-    #     "CO2 removal service",
-    #     bus="co2 atmosphere",
-    #     carrier="CO2 removal service",
-    #     p_nom=1e6,
-    #     marginal_cost=1000, # marginal cost in €/tCO2
-    #     p_nom_extendable=False,
-    #     p_min_pu=0,
-    #     p_max_pu=1.0,
-    #     sign=-1,
-    # )
 
     snakemake.params.solving["options"]["noisy_costs"] = False
 
     # TODO remove this again
-    snakemake.params.solving["options"]["load_shedding"] = 100
-
-    results = {}
-    crazy = {}
-    for addon in np.logspace(0, 3, 10):
-        m = n.copy()
-        m.global_constraints.loc[["CO2Limit"], "constant"] += addon
-
-        prepare_network(
-            m,
-            solve_opts=snakemake.params.solving["options"],
-            foresight=snakemake.params.foresight,
-            planning_horizons=planning_horizons,
-            co2_sequestration_potential=snakemake.params["co2_sequestration_potential"],
-            limit_max_growth=snakemake.params.get("sector", {}).get("limit_max_growth"),
-            regret_run=snakemake.params.get("regret_run", False),
-        )
-        with memory_logger(
-            filename=getattr(snakemake.log, "memory", None), interval=logging_frequency
-        ) as mem:
-            solve_network(
-                m,
-                config=snakemake.config,
-                params=snakemake.params,
-                solving=snakemake.params.solving,
-                planning_horizons=planning_horizons,
-                rule_name=snakemake.rule,
-                log_fn=snakemake.log.solver,
-                snakemake=snakemake,
-            )
-        logger.info(f"Maximum memory usage: {mem.mem_usage}")
-        results[addon] = m.global_constraints.loc["CO2Limit", "mu"]
-        crazy[addon] = (
-            decision.global_constraints.loc["CO2Limit", "constant"]
-            - m.global_constraints.loc["CO2Limit", "constant"]
-        )
-
-    n.global_constraints.loc[["CO2Limit"], "constant"] += 1000
-    # n.global_constraints.loc[["co2_sequestration_limit"], "constant"] -= 100
+    # snakemake.params.solving["options"]["load_shedding"] = 100
 
     prepare_network(
         n,
@@ -326,8 +241,20 @@ if __name__ == "__main__":
         planning_horizons=planning_horizons,
         co2_sequestration_potential=snakemake.params["co2_sequestration_potential"],
         limit_max_growth=snakemake.params.get("sector", {}).get("limit_max_growth"),
-        regret_run=snakemake.params.get("regret_run", False),
+        regret_run=True,  # snakemake.params.get("regret_run", False),
     )
+
+    # n.add(
+    #     "Generator",
+    #     "co2 atmosphere",
+    #     bus="co2 atmosphere",
+    #     p_min_pu=-1,
+    #     p_max_pu=0,
+    #     p_nom_extendable=True,
+    #     marginal_cost=realization.global_constraints.loc["CO2Limit", "mu"],
+    # )
+
+    # n.global_constraints.drop("CO2Limit", inplace=True)
 
     with memory_logger(
         filename=getattr(snakemake.log, "memory", None), interval=logging_frequency
@@ -354,17 +281,6 @@ if __name__ == "__main__":
             n.links.query("carrier == 'electricity distribution grid'").p_nom_opt
             - decision.links.query(
                 "carrier == 'electricity distribution grid'"
-            ).p_nom_opt
-        ).sort_values()
-    )
-
-    logger.info(
-        (
-            decision.links.query(
-                "carrier == 'waste CHP' and index.str.startswith('DE')"
-            ).p_nom_opt
-            - n.links.query(
-                "carrier == 'waste CHP' and index.str.startswith('DE')"
             ).p_nom_opt
         ).sort_values()
     )
