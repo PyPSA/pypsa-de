94dbc094f207087fce7413e9e42c6d10ed50140c
Michael Lindner <michaellindner@posteo.de>
2025-09-10 17:02:48 +0200

    fix type of year

--- scripts/pypsa-de/build_exogenous_mobility_data.py
+++ scripts/pypsa-de/build_exogenous_mobility_data.py
@@ -62,7 +62,7 @@ def get_mobility_data(
             # FZ27_202101, table FZ 27.2, 1. January 2021:
             mobility_data["million_EVs"] = 0.358498 + 0.280149
 
-    elif year == "2025" and year in uba_for_mobility:
+    elif year == "2025" and int(year) in uba_for_mobility:
         # https://www.umweltbundesamt.de/sites/default/files/medien/11850/publikationen/projektionsbericht_2025.pdf, Abbildung 64 & 59,
         mobility_data = pd.Series(
             {
@@ -77,7 +77,7 @@ def get_mobility_data(
         mobility_data = mobility_data.mul(1e6)  # convert TWh to MWh
         mobility_data["million_EVs"] = 2.7 + 1.2  # BEV + PHEV
 
-    elif year == "2030" and year in uba_for_mobility:
+    elif year == "2030" and int(year) in uba_for_mobility:
         mobility_data = pd.Series(
             {
                 "Electricity": 57.0,
@@ -89,7 +89,7 @@ def get_mobility_data(
         mobility_data = mobility_data.mul(1e6)
         mobility_data["million_EVs"] = 8.7 + 1.8
 
-    elif year == "2035" and year in uba_for_mobility:
+    elif year == "2035" and int(year) in uba_for_mobility:
         mobility_data = pd.Series(
             {
                 "Electricity": 117.0,
@@ -102,7 +102,7 @@ def get_mobility_data(
         mobility_data["million_EVs"] = 18.9 + 1.8
 
     else:
-        if year in uba_for_mobility:  # here year > 2035
+        if int(year) in uba_for_mobility:  # here year > 2035
             logger.error(
                 f"Year {year} is not supported for UBA mobility projections. Please use only 2020, 2025, 2030, 2035."
             )
@@ -132,8 +132,8 @@ if __name__ == "__main__":
             opts="",
             ll="vopt",
             sector_opts="none",
-            planning_horizons="2020",
-            run="KN2045_Mix",
+            planning_horizons="2025",
+            run="LowDemand",
         )
     configure_logging(snakemake)
 
