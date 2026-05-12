94b0657e5d6a662bfa47975abc9530c8ef474d45
Michael Lindner <michaellindner@posteo.de>
2026-05-12 14:30:40 +0200

    add grid reinforcement costs to solar rooftop as well

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -1880,7 +1880,7 @@ def insert_gas_distribution_costs(
 
 
 def add_electricity_grid_connection(n, costs):
-    carriers = ["onwind", "solar", "solar-hsat"]
+    carriers = ["onwind", "solar", "solar-hsat", "solar rooftop"]
 
     gens = n.generators.index[n.generators.carrier.isin(carriers)]
 
