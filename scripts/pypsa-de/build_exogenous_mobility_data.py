27d09440486bd3bff07f55c419c035c8fc4ee8d6
Michael Lindner <michaellindner@posteo.de>
2026-08-10 16:44:57 +0200

    use UBA data for 2030 transport

--- scripts/pypsa-de/build_exogenous_mobility_data.py
+++ scripts/pypsa-de/build_exogenous_mobility_data.py
@@ -87,18 +87,22 @@ def get_mobility_data(
         mobility_data["million_EVs"] = 2.034260 + 1.112958
 
     elif year == "2030" and int(year) in uba_for_mobility:
+        # https://www.umweltbundesamt.de/system/files/medien/11850/publikationen/2026-07/Projektionsbericht_2026_V2.pdf, Abb 59 & 54
         mobility_data = pd.Series(
             {
-                "Electricity": 57.0,
-                "Hydrogen": 14.0,
-                "Liquids": 418.0 + 34.0 + 1.0,
+                "Electricity": 41.0,
+                "Hydrogen": 0.0,
+                "Liquids": 429.0 + 61,
             }
         )
-        mobility_data["Liquids"] -= non_land_liquids[int(year)]
+        mobility_data["Liquids"] -= non_land_liquids[
+            int(year)
+        ]  # remove domestic navigation and aviation from UBA data to avoid double counting
         mobility_data = mobility_data.mul(1e6)
-        mobility_data["million_EVs"] = 8.7 + 1.8
+        mobility_data["million_EVs"] = 7 + 2  # BEV + PHEV
 
     elif year == "2035" and int(year) in uba_for_mobility:
+        # https://www.umweltbundesamt.de/sites/default/files/medien/11850/publikationen/projektionsbericht_2025.pdf, Abbildung 64 & 59,
         mobility_data = pd.Series(
             {
                 "Electricity": 117.0,
